30f485eff4cd405bb2511d67887665c3e5dc79cf
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-12-17 19:25:07 +0000

    ci work

---
+++ integration_tests/module.py
@@ -0,0 +1,21 @@
+import cxone_api as cx
+from cxone_api.low.scans import retrieve_list_of_scans
+from cxone_api.util import json_on_ok
+from cxone_sarif import get_sarif_v210_log_for_scan
+from cxone_sarif.opts import DEFAULT
+import os, asyncio
+
+
+async def main():
+  client = cx.CxOneClient.create_with_oauth(os.environ['CXONE_CLIENT_ID'], os.environ['CXONE_CLIENT_SECRET'], "ghaction", 
+    cx.AuthUS(os.environ['CXONE_TENANT']), cx.ApiUS())
+  
+  scans = json_on_ok(await retrieve_list_of_scans(client, limit=1, statuses="Completed"))
+  scan_ids = [x['id'] for x in scans['scans']]
+
+  sarif_log = await get_sarif_v210_log_for_scan(client, DEFAULT, scan_ids[0], True)
+
+  exit (0 if sarif_log is not None else 1)
+
+if __name__ == "__main__":
+  asyncio.run(main())
