4d8fb629a1b4cd662e20489e747c7a3544978f50
Nathan Leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-12-17 19:28:01 +0000

    ci work

--- integration_tests/module.py
+++ integration_tests/module.py
@@ -3,7 +3,7 @@ from cxone_api.low.scans import retrieve_list_of_scans
 from cxone_api.util import json_on_ok
 from cxone_sarif import get_sarif_v210_log_for_scan
 from cxone_sarif.opts import DEFAULT
-import os, asyncio
+import os, asyncio,json
 
 
 async def main():
@@ -14,6 +14,8 @@ async def main():
   scan_ids = [x['id'] for x in scans['scans']]
 
   sarif_log = await get_sarif_v210_log_for_scan(client, DEFAULT, scan_ids[0], True)
+  j = json.loads(sarif_log.asjson())
+  print(json.dumps(j, indent=2))
 
   exit (0 if sarif_log is not None else 1)
 
