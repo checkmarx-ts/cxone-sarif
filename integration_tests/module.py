7adf277a6dce3f6f1bc3d801db2f78e465d8c9ca
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-06-05 07:43:47 -0500

    change error checks

--- integration_tests/module.py
+++ integration_tests/module.py
@@ -14,10 +14,11 @@ async def main():
   scan_ids = [x['id'] for x in scans['scans']]
 
   sarif_log = await get_sarif_v210_log_for_scan(client, DEFAULT, scan_ids[0], True)
-  j = json.loads(sarif_log.asjson())
-  print(json.dumps(j, indent=2))
-
-  exit (0 if sarif_log is not None else 1)
+  if sarif_log is not None:
+    j = json.loads(sarif_log.asjson())
+    print(json.dumps(j, indent=2))
+  else:
+    exit(1)
 
 if __name__ == "__main__":
   asyncio.run(main())
